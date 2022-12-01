63ad8f74f35f495043497efe76d28b0c21ca1e1f
sebastianocalella <sebastianocalella97@gmail.com>
2022-12-01 12:45:17 +0100

    getRandomNumber function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const gridElement = document.getElementById('grid');
 const startElement = document.getElementById('play');
 const mainElement = document.getElementById('main-element');
 
+const bombList = [];
+
 
 startElement.addEventListener('click', function(){
     gridElement.innerHTML = "";
@@ -12,6 +14,19 @@ startElement.addEventListener('click', function(){
         gridElement.appendChild(newSquare);
     }
 })
+
+
+//******************************************************************** */
+
+
+//******************************************************************** */
+
+function getRandomNumber(numMin, numMax){
+    if(numMin===numMax){
+        return numMax
+    }
+    return Math.floor(Math.random()*(numMax - numMin + 1) + numMin);
+}
 
 function createSquare (){
     const DivElement = document.createElement('div');
